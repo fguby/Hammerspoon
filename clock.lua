6bbb5b579ffe425053037fbf7fb5abe44a38545d
fguby <fguby1995@gmail.com>
2020-02-04 11:31:54 +0800

    update timer

--- clock.lua
+++ clock.lua
@@ -3,9 +3,9 @@ hs.hotkey.bind({"ctrl"}, "T", function()
   	if(a1 == "确定")
   		then
   			hs.alert.show("Success(..•˘_˘•..)")
-  			clock = hs.menubar.new()
-  			t = string.sub(a2, -1)
-  			time = tonumber(string.sub(a2, 1, string.len(a2) - 1))
+  			local clock = hs.menubar.new()
+  			local t = string.sub(a2, -1)
+  			local time = tonumber(string.sub(a2, 1, string.len(a2) - 1))
   			if(t == "m")
   				then
   					time = 60 * time
@@ -13,17 +13,28 @@ hs.hotkey.bind({"ctrl"}, "T", function()
 				then
 					time = 60 * 60 * time
 			end
-			start = 0
+			local start = 0
+			local icon = hs.image.imageFromPath("~/icon/bird.png")
+			local icon = icon:setSize({w=20, h=20})
+			clock:setIcon(icon, false)
   			hs.timer.doUntil(function()
   				-- body
   				if(start >= time)
   					then
+  						s = hs.sound.getByName("Glass.aiff")
+  						s:play()
 						clock:delete()
-						hs.alert.show("定时器时间截止~")  
+						hs.alert.show("到时间啦(..•˘_˘•..)")  
   						return true
 				end
   			end, function()
-  				clock:setTitle(time-start)
+  				local seconds = time - start
+  				local hour = math.floor(seconds/3600)
+  				local minutes = math.floor((seconds - hour*60)/60)
+  				local lastSeconds = seconds - hour*3600 - minutes * 60
+  				local title = string.format(" %02d:%02d:%02d", hour, minutes, lastSeconds)
+          title = hs.styledtext.ansi(title)
+  				clock:setTitle(title)
   				start = start + 1
   			end)
 		end
